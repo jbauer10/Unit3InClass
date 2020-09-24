a308741a7606364341918165f7c451d46708a9a5
jbauer <jbauer10@css.edu>
2020-09-24 10:36:52 -0500

    Changed

---
+++ Unit3InClass/Sales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3InClass
+{
+    class Sales
+    {
+        DateTime saleTime;
+        float amount;
+    }
+
+
+}
